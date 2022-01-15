4e6c5ddb005ecfd66ee8dfc4087f182da1e77c63
Zaremauseinudja <zaremka7@mail.ru>
2022-01-15 20:28:14 +0300

    lesson 2, init project

---
+++ lesson_0.py
@@ -0,0 +1,81 @@
+a = 'Hello wold'
+print (a)
+
+b = 111
+print (b)
+
+c = 1.1
+
+print (c)
+
+d = bytes('bytes', encoding = 'utf-8')
+
+print (d)
+
+e = [1,2,3,4,5]
+
+print (e)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+print(type(thistuple))
+
+
+
+thisset = {"apple", "banana", "cherry"}
+print(thisset)
+
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+print (thistuple)
+print (thisset)
+
+frozenset = frozenset(['cat', 'dog'])
+print(frozenset)
+
+d1 = 'dict'
+
+dictofon = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+}
+
+print (dictofon['brand'])
+
+print(type(a))
+print (type(b))
+print (type(c))
+print (type(c))
+print (type(d))
+print (type(e))
+print (type(thistuple))
+print (type(thisset))
+print (type(frozenset))
+print (type(dict))
+
+
+
+intf = 'interface'
+tun = 'Tunnel0'
+
+print (intf + tun)
+
+print (type(intf))
+
+kiki = "a"
+loko = b
+
+print ('a' , b)
+
+lichi = "a"
+tichi = b
+
+print ('a' , + b)
+
+
+
+
